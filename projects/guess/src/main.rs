026497d6e537d48c30ce542c82bb73a54401101b
hRebb <hamza.rebb@gmail.com>
2024-03-19 12:17:56 +0000

    complete the first program of the book

--- projects/guess/src/main.rs
+++ projects/guess/src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number");
-    println!("Enter your solution :");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin
+    io::stdin()
         .read_line(&mut guess)
-        .expect("You failed");
+        .expect("Failed to read line");
 
-    println!("You guessed : {guess}");
+    println!("You guessed: {guess}");
 }
