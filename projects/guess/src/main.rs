a5576a142f6938076a9cc6154028e7ac2fa0f2ed
hRebb <hamza.rebb@gmail.com>
2024-03-20 11:19:57 +0000

    update package

--- projects/guess/src/main.rs
+++ projects/guess/src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
@@ -12,4 +14,5 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+    println!("The secret number is: {secret_number}")
 }
