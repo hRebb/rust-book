d685ee0b8ef67aa40a1f718157409095a38ae6a0
hRebb <hamza.rebb@gmail.com>
2024-03-20 11:43:02 +0000

    finish the guess script

--- projects/guess/src/main.rs
+++ projects/guess/src/main.rs
@@ -1,13 +1,13 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-   loop
-   {
+    loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -16,21 +16,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue;
+            Err(_) => continue,
         };
 
         println!("You guessed: {guess}");
-        println!("The secret number is: {secret_number}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small"),
-            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win");
+                println!("You win!");
                 break;
             }
         }
-   }
+    }
 }
