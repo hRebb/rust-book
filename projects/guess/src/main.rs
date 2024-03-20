0f1e1ed69f7b0168ee2bffad70496fbcb9eb68e8
hRebb <hamza.rebb@gmail.com>
2024-03-20 11:34:51 +0000

    Ajouter la comparaison de devinette et afficher le résultat

--- projects/guess/src/main.rs
+++ projects/guess/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -13,6 +14,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guessed: {guess}");
-    println!("The secret number is: {secret_number}")
+    println!("The secret number is: {secret_number}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win")
+    }
 }
